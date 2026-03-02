8cacfc738f9619bbe7e0a5d6810acec5509109f0
Dein NameCornelliX <cornelius.koerner@googlemail.com>
2026-03-02 01:14:49 +0100

    PWA: manifest, service worker, icons

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import { renderApp } from './app.js'
 
 const app = document.getElementById('app')
 renderApp(app)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/konzert-app/sw.js')
+}
