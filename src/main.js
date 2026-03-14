5fca15e51b381d3cc9b8dbaafe4707e24d598759
Dein NameCornelliX <cornelius.koerner@googlemail.com>
2026-03-14 21:54:00 +0100

    Fix: deleteOldManualEvents vor renderApp

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { renderApp } from './app.js'
 import { deleteOldManualEvents } from './supabase.js'
 
 const app = document.getElementById('app')
-renderApp(app)
-deleteOldManualEvents()
+
+deleteOldManualEvents().then(() => {
+  renderApp(app)
+})
 
 if ('serviceWorker' in navigator && location.hostname !== 'localhost') {
   navigator.serviceWorker.register('/konzert-app/sw.js')
